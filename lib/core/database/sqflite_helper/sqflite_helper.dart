81fbee495a79ad0cc405f3f655e90b1d21f7fcaa
tala <foqhat835@gmail.com>
2025-08-01 14:24:15 +0300

    sqflite done

---
+++ lib/core/database/sqflite_helper/sqflite_helper.dart
@@ -0,0 +1,39 @@
+// core/database/sqflite_helper/sqflite_helper.dart
+import 'package:sqflite/sqflite.dart';
+
+class SqfliteHelper {
+  late Database db;
+  // create Db
+  //2.create table
+  // 3.CRUD =>create-read-update-delete
+
+  void initDB()async{
+   await openDatabase('tasks.db',
+
+   version: 1,
+  
+   onCreate: (db, version)async {
+    // --step 2 create table
+  await  db.execute('''
+CREATE TABLE Tasks(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+title TEXT,
+notes TEXT,
+date Text,
+startTime TEXT,
+endTime Text,
+color INTEGER,
+complete INTEGER
+)
+''').then((value)=>print('DB created successfully'),
+
+);
+
+   
+     
+   },
+    onOpen: (db) {
+     print('DB oppned');}
+   ).then((value)=>db=value).catchError((e){print(e.toString());});
+  }
+}
