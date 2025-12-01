1baa4452c6fb0f487b4c893bfc69869bae1c0ef0
tala <foqhat835@gmail.com>
2025-12-01 20:12:45 +0200

    done

--- lib/core/services/services_locator.dart
+++ lib/core/services/services_locator.dart
@@ -1,10 +1,14 @@
 // core/services/services_locator.dart
 import 'package:get_it/get_it.dart';
 import 'package:to_do_app/core/database/cache/cache_helper.dart';
+import 'package:to_do_app/core/database/sqflite_helper/sqflite_helper.dart';
 final getIt = GetIt.instance;
 
 void setup() {
   getIt.registerLazySingleton<CacheHelper>(()=>CacheHelper());
+
+    getIt.registerLazySingleton<SqfliteHelper>(()=>SqfliteHelper());
+
 
 
 }
